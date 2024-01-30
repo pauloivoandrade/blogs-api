added9499c2ea214fc79a6e60dd1b2cf7417fdfc
pauloivoandrade <pauloivoasantos@gmail.com>
2024-01-30 15:47:27 -0300

    modelo User em src/models/User.js

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+const { date } = require("joi")
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+        'User',
+        {
+            id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+            displayName: { type: DataTypes.STRING, field: 'display_name' },
+            email: DataTypes.STRING,
+            password: DataTypes.STRING,
+            image: DataTypes.STRING,
+        },
+        {
+            timestamps: false,
+            underscored: true,
+        },
+    );
+    return User;
+}
